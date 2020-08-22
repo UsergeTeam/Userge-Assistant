0664c7dc0d2e8a0c52c16cda6dbed6bb39af206b
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-08-22 21:16:39 +0530

    adding alive.py

---
+++ assistant/plugins/alive.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+#
+# This file is part of < https://github.com/UsergeTeam/Userge-Assistant > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Userge-Assistant/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import time
+
+from pyrogram import Message, Filters
+from pyrogram.errors.exceptions import FileIdInvalid, FileReferenceEmpty
+from pyrogram.errors.exceptions.bad_request_400 import BadRequest
+
+from assistant import bot, Config, versions
+from assistant.utils import START_TIME, time_formatter
+
+LOGO_STICKER_ID, LOGO_STICKER_REF = None, None
+
+
+@bot.on_message(Config.AUTH_CHATS & Filters.command("alive"))
+async def _alive(_, message: Message):
+    try:
+        if LOGO_STICKER_ID:
+            await sendit(LOGO_STICKER_ID, LOGO_STICKER_REF, message)
+        else:
+            await refresh_id()
+            await sendit(LOGO_STICKER_ID, LOGO_STICKER_REF, message)
+    except (FileIdInvalid, FileReferenceEmpty, BadRequest):
+        await refresh_id()
+        await sendit(LOGO_STICKER_ID, LOGO_STICKER_REF, message)
+    output = f"""
+**Userge-Assistant is Up**
+• **uptime** : `{time_formatter(time.time() - START_TIME)}`
+• **python version** : `{versions.__python_version__}`
+• **pyrogram version** : `{versions.__pyro_version__}`
+• **bot version** : `v1.0`
+• **license** : {versions.__license__}
+• **copyright** : {versions.__copyright__}
+• **repo** : [Userge-Assistant](https://github.com/UsergeTeam/Userge-Assistant)
+"""
+    await bot.send_message(message.chat.id, output, disable_web_page_preview=True)
+
+
+async def refresh_id():
+    global LOGO_STICKER_ID, LOGO_STICKER_REF
+    sticker = (await bot.get_messages('UsergeOt', 488248)).sticker
+    LOGO_STICKER_ID = sticker.file_id
+    LOGO_STICKER_REF = sticker.file_ref
+
+
+async def sendit(fileid, fileref, message: Message):
+    await bot.send_sticker(message.chat.id, fileid, file_ref=fileref)
