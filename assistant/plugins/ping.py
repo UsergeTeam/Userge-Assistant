94762716dc26c6168991cb7e0986e8cff86ca572
rking32 <sljohnwick@gmail.com>
2020-08-22 17:35:10 +0530

    fix spamming recent actions

--- assistant/plugins/ping.py
+++ assistant/plugins/ping.py
@@ -6,7 +6,6 @@
 #
 # All rights reserved.
 
-import asyncio
 from datetime import datetime
 
 from pyrogram import Message, Filters
@@ -21,5 +20,3 @@ async def _ping(_, message: Message):
     end = datetime.now()
     m_s = (end - start).microseconds / 1000
     await replied.edit(f"**Pong!**\n`{m_s} ms`")
-    await asyncio.sleep(5)
-    await replied.delete()
