6435ab8b491b7f17c02b57f4424016ee2ce17c9f
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-08-22 21:11:19 +0530

    some change

--- assistant/logger.py
+++ assistant/logger.py
@@ -6,11 +6,26 @@
 #
 # All rights reserved.
 
+import time
 import logging
 
+START_TIME = time.time()
+
+# enable logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+def time_formatter(seconds: float) -> str:
+    """ humanize time """
+    minutes, seconds = divmod(int(seconds), 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "")
+    return tmp[:-2]
