4026c71f3a9a68a40c8d54d676ea2647728bcbd7
rking32 <52490534+rking32@users.noreply.github.com>
2020-08-30 16:08:55 +0530

    Update report.py

--- assistant/plugins/report.py
+++ assistant/plugins/report.py
@@ -15,22 +15,20 @@ from assistant import bot, cus_filters, Config
 @bot.on_message(filters.command("report") & cus_filters.auth_chats)
 async def _report(_, msg: Message):
     replied = msg.reply_to_message
-    cmd = len(msg.text)
     if not replied:
         return
-    if msg.text and cmd == 7:
+    if len(msg.text) < 9:
         return
     _, args = msg.text.split(maxsplit=1)
-    if replied and args:
-        reason = f"**Reported User:** {replied.from_user.mention}\n"
-        reason += f"**Reported Msg link:** {replied.link}\n"
-        reason += f"**Reason:** `{args}`\n\n"
-        reason += f"**Report From:** {msg.from_user.mention}"
-    admins = Config.ADMINS.get(msg.chat.id)
-    for i in range(len(admins)):
+    if not args:
+        return
+    reason = f"**Reported User:** {replied.from_user.mention}\n"
+    reason += f"**Reported Msg link:** {replied.link}\n"
+    reason += f"**Reason:** `{args}`\n\n"
+    reason += f"**Report From:** {msg.from_user.mention}"
+    for admin_id in Config.ADMINS.get(msg.chat.id):
         try:
-            await bot.send_message(
-                admins[i], reason, disable_web_page_preview=True)
+            await bot.send_message(admin_id, reason, disable_web_page_preview=True)
         except Exception:  # pylint: disable=broad-except
             pass
-    await msg.reply("`Report request Accepted...`")
+    await msg.reply("`Report request Accepted ...`")
