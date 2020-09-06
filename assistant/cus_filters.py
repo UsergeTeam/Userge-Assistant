11787b966260bd9b41f55193be542d6b8f5d42c9
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-09-06 15:25:29 +0530

    added filter for whitelist chats

--- assistant/cus_filters.py
+++ assistant/cus_filters.py
@@ -45,5 +45,12 @@ async def _is_admin_or_dev(_, bot: Client, msg: Message) -> bool:
     return msg.from_user.id in Config.ADMINS[msg.chat.id]
 
 
+async def _is_chat_whitelist(_, msg: Message) -> bool:
+    if msg.chat.id in Config.WHITELIST_CHATS:
+        return False
+    return True
+
+
 auth_chats = filters.chat(list(Config.AUTH_CHATS))
 auth_users = filters.create(_is_admin_or_dev)
+whitelist_chats = filters.create(_is_chat_whitelist)
