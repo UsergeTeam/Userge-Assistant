1fc08e4a716da41abcd4a7ac8613786f015012aa
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-11-08 18:06:41 +0530

    Update inline_docs.py

--- assistant/plugins/inline_docs.py
+++ assistant/plugins/inline_docs.py
@@ -13,9 +13,6 @@ from pyrogram.types import (
 
 from assistant import bot
 from assistant.utils import docs
-from assistant.utils.docs import (
-    USERGE, DECORATORS, DEPLOYMENT,
-    VARS, MODES, EXAMPLES, FAQS)
 
 
 @bot.on_inline_query()
@@ -24,10 +21,207 @@ async def inline_docs(_, i_q: InlineQuery):
 
     if query == "":
         await i_q.answer(
-            results=USERGE,
+            results=docs.USERGE,
             cache_time=5,
-            switch_pm_text=f"{emoji.MAGNIFYING_GLASS_TILTED_RIGHT} Type to search UserGe Docs",
+            switch_pm_text="🔍 Type to search UserGe Docs",
             switch_pm_parameter="start",
         )
 
         return
+    results = []
+    if query == "decorators":
+        results.append(
+            InlineQueryResultArticle(
+                title="Decorators",
+                description="UserGe Decorators online documentation page",
+                input_message_content=InputTextMessageContent(
+                    f"{docs.intro}"
+                    f"`Online Documentation Page for UserGe Decorators.`"
+                ),
+                reply_markup=InlineKeyboardMarkup([[
+                    InlineKeyboardButton(
+                        "📚 Online docs",
+                        url=docs.decorators
+                    )
+                ]]),
+                thumb_url=docs.USERGE_THUMB,
+            )
+        )
+
+        for i in docs.DECORATORS:
+            results.append(
+                InlineQueryResultArticle(
+                    title=i[0],
+                    description=i[1],
+                    input_message_content=InputTextMessageContent(
+                        docs.intro + i[2]
+                    ),
+                    thumb_url=docs.DECORATORS_THUMB
+                )
+            )
+    elif query == "deployment":
+        results.append(
+            InlineQueryResultArticle(
+                title="Deployment",
+                description="UserGe Deployment online documentation page",
+                input_message_content=InputTextMessageContent(
+                    f"{docs.intro}"
+                    f"`Online Documentation Page for UserGe Deployment.`"
+                ),
+                reply_markup=InlineKeyboardMarkup([[
+                    InlineKeyboardButton(
+                        "📚 Online docs",
+                        url=docs.deployment
+                    )
+                ]]),
+                thumb_url=docs.USERGE_THUMB,
+            )
+        )
+
+        for i in docs.DEPLOYMENT:
+            results.append(
+                InlineQueryResultArticle(
+                    title=i[0],
+                    description=i[1],
+                    input_message_content=InputTextMessageContent(
+                        docs.intro + i[2]
+                    ),
+                    thumb_url=docs.DEPLOYMENT_THUMB
+                )
+            )
+    elif query == "vars":
+        results.append(
+            InlineQueryResultArticle(
+                title="VARS",
+                description="UserGe Vars online documentation page",
+                input_message_content=InputTextMessageContent(
+                    f"{docs.intro}"
+                    f"`Online Documentation Page for UserGe Vars.`"
+                ),
+                reply_markup=InlineKeyboardMarkup([[
+                    InlineKeyboardButton(
+                        "📚 Online docs",
+                        url=docs.vars
+                    )
+                ]]),
+                thumb_url=docs.USERGE_THUMB,
+            )
+        )
+
+        for i in docs.vars:
+            results.append(
+                InlineQueryResultArticle(
+                    title=i[0],
+                    description=i[1],
+                    input_message_content=InputTextMessageContent(
+                        docs.intro + i[2]
+                    ),
+                    thumb_url=docs.VARS_THUMB
+                )
+            )
+    elif query == "modes":
+        results.append(
+            InlineQueryResultArticle(
+                title="UserGe Modes",
+                description="UserGe Modes online documentation page",
+                input_message_content=InputTextMessageContent(
+                    f"{docs.intro}"
+                    f"`Online Documentation Page for UserGe Modes.`"
+                ),
+                reply_markup=InlineKeyboardMarkup([[
+                    InlineKeyboardButton(
+                        "📚 Online docs",
+                        url=docs.modes
+                    )
+                ]]),
+                thumb_url=docs.USERGE_THUMB,
+            )
+        )
+
+        for i in docs.MODES:
+            results.append(
+                InlineQueryResultArticle(
+                    title=i[0],
+                    description=i[1],
+                    input_message_content=InputTextMessageContent(
+                        docs.intro + i[2]
+                    ),
+                    thumb_url=i[3]
+                )
+            )
+    elif query == "example":
+        results.append(
+            InlineQueryResultArticle(
+                title="UserGe Example",
+                description="UserGe Example-Plugins online documentation page",
+                input_message_content=InputTextMessageContent(
+                    f"{docs.intro}"
+                    f"`Online Documentation Page for UserGe Example-Plugins.`"
+                ),
+                reply_markup=InlineKeyboardMarkup([[
+                    InlineKeyboardButton(
+                        "📚 Online docs",
+                        url=docs.example
+                    )
+                ]]),
+                thumb_url=docs.USERGE_THUMB,
+            )
+        )
+
+        for i in docs.EXAMPLE:
+            results.append(
+                InlineQueryResultArticle(
+                    title=i[0],
+                    description=i[1],
+                    input_message_content=InputTextMessageContent(
+                        docs.intro + i[2]
+                    ),
+                    thumb_url=docs.EXAMPLE_THUMB
+                )
+            )
+    elif query in ["faq", "faqs"]:
+        results.append(
+            InlineQueryResultArticle(
+                title="UserGe-FAQs",
+                description="UserGe-FAQs online documentation page",
+                input_message_content=InputTextMessageContent(
+                    f"{docs.intro}"
+                    f"`Online Documentation Page for UserGe-FAQs.`"
+                ),
+                reply_markup=InlineKeyboardMarkup([[
+                    InlineKeyboardButton(
+                        "📚 Online docs",
+                        url=docs.faqs
+                    )
+                ]]),
+                thumb_url=docs.USERGE_THUMB,
+            )
+        )
+
+        for i in range(len(docs.FAQS)):
+            results.append(
+                InlineQueryResultArticle(
+                    title=f"FAQ {i+1}",
+                    description=docs.FAQS[i][0],
+                    input_message_content=InputTextMessageContent(
+                        f"{docs.intro}FAQ {i+1}:-\n"
+                        f"[{docs.FAQS[i][0]}]({docs.FAQS[i][1]})
+                    ),
+                    thumb_url=docs.FAQS_THUMB
+                )
+            )
+    if results:
+        switch_pm_text = f"📖 {len(results)} Results for \"{query}\""
+        await i_q.answer(
+            results=results,
+            cache_time=5,
+            switch_pm_text=switch_pm_text,
+            switch_pm_parameter="start"
+        )
+    else:
+        await i_q.answer(
+                results=[],
+                cache_time=5,
+                switch_pm_text=f'❌ No results for "{query}"',
+                switch_pm_parameter="okay"
+            )
