86920f99f12cd67dab97279d23f223beb2f54490
kaj117 <70688258+kaj117@users.noreply.github.com>
2020-11-21 18:16:30 +0530

    Update config.py

--- assistant/config.py
+++ assistant/config.py
@@ -18,20 +18,17 @@ if os.path.isfile("config.env"):
 
 class Config:
     """ assistant configs """
-    APP_ID = int(os.environ.get("APP_ID", 0))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    AUTH_CHATS = set([-1001481357570])  # @UserGeOt
+    APP_ID = int(os.environ.get("1383845", 0))
+    API_HASH = os.environ.get("0e3d2c299cc3c5cc26c283cecd2eb97c")
+    BOT_TOKEN = os.environ.get("1138227910:AAHRh2gfBE5eHdDJCpEhyvD0-jLjfNdiN7s")
+    AUTH_CHATS = set([-1001266398622])  # @UserGeOt
     if os.environ.get("AUTH_CHATS"):
         AUTH_CHATS.update(map(int, os.environ.get("AUTH_CHATS").split()))
-    WHITELIST_CHATS = set([-1001465749479])  # @UserGeSpam
+    WHITELIST_CHATS = set([-1001470978862])  # @UserGeSpam
     if os.environ.get("WHITELIST_CHATS"):
         WHITELIST_CHATS.update(map(int, os.environ.get("WHITELIST_CHATS").split()))
     DEV_USERS = (
-        1158855661,  # @Krishna_Singhal
-        1110621941,  # @PhycoNinja13b
-        921420874,   # @juznem
-        837784353    # @rking_32
+        1131653685
     )
     ADMINS = {}
     MAX_MSG_LENGTH = 4096
