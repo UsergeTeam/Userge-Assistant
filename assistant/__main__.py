6ddda928dd5ed40842a55a8f7e20605f2ed067bf
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2021-01-17 15:53:04 +0530

    Reverted Loader

--- assistant/__main__.py
+++ assistant/__main__.py
@@ -6,60 +6,7 @@
 #
 # All rights reserved.
     
-import os
-import asyncio
-import importlib
+from assistant import bot
 
-from pyrogram import idle
-
-from assistant import bot, Config, DB, logging
-
-_LOG = logging.getLogger(__name__)
-
-
-async def _loader():
-    if len(Config.PLUGINS_ID) > 0:
-        msg = await bot.get_messages(DB.CHANNEL_ID, Config.PLUGINS_ID)
-        _LOG.info("Loading Temp PLugins...")
-        plg_list = []
-        
-        for i in range(len(Config.PLUGINS_ID)):
-            file = msg[i]
-            document = file.document
-            if file and document:
-                if document.file_name.endswith('.py') and document.file_size < 2 ** 20:
-                    path = "Userge-Assistant/assistant/plugins/"
-                    if not os.path.isdir(path):
-                        os.makedirs(path)
-                    t_path = path + document.file_name
-                    if os.path.isfile(t_path):
-                        os.remove(t_path)
-                    await file.download(file_name=t_path)
-                    plugin = '.'.join(t_path.split('/'))[:-3]
-                    try:
-                        load_plugin(plugin)
-                    except Exception:
-                        os.remove(t_path)
-                    else:
-                        plg_list.append(document.file_name[:-3])
-        _LOG.info(f"Loaded Plugins: {plg_list}")
-
-
-def load_plugin(name: str):
-    try:
-        importlib.import_module(name)
-    except ImportError as i_e:
-        _LOG.error(i_e)
-        raise
-
-
-async def main():
-    await bot.start()
-    await _loader()
-    await idle()
-    await bot.stop()
-
-    
 if __name__ == "__main__":
-    _LOG.info("Starting Assistant Bot!")
-    asyncio.run(main())
+    bot.run()
